31debf6ea092fb140569671464a4df143bf798aa
AntoniooBagurr <abagur@algebra.hr>
2024-12-11 13:10:21 +0100

    JWT

--- PeerTutoringNetwork/PeerTutoringNetwork/Controllers/ReservationController.cs
+++ PeerTutoringNetwork/PeerTutoringNetwork/Controllers/ReservationController.cs
@@ -5,11 +5,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using PeerTutoringNetwork.DTOs;
+using Microsoft.AspNetCore.Authorization;
 
 namespace PeerTutoringNetwork.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ReservationController : ControllerBase
     {
         private readonly PeerTutoringNetworkContext _context;
---
+++ PeerTutoringNetwork/PeerTutoringNetwork/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PeerTutoringNetwork.Security;
+
+namespace PeerTutoringNetwork.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("[action]")]
+        public ActionResult GetToken()
+        {
+            try
+            {
+                // The same secure key must be used here to create JWT,
+                // as the one that is used by middleware to verify JWT
+                var secureKey = _configuration["JWT:SecureKey"];
+                var serializedToken = JwtTokenProvider.CreateToken(secureKey, 10);
+
+                return Ok(serializedToken);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
