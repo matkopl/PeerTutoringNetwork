9f6bd15284441bf24da29309b596fb3f8825c75d
AntoniooBagurr <abagur@algebra.hr>
2024-12-11 19:40:27 +0100

    Reapply "Registracija"
    This reverts commit 92a4a924ee879fc80a9be2bfe22e8b22bbf138af.

--- PeerTutoringNetwork/PeerTutoringNetwork/Models/User.cs
+++ PeerTutoringNetwork/PeerTutoringNetwork/Models/User.cs
@@ -9,19 +9,19 @@ public partial class User
 
     public string Username { get; set; } = null!;
 
-    public string PwdHash { get; set; } = null!;
+    public byte[] PwdHash { get; set; } = null!;
 
-    public string PwdSalt { get; set; } = null!;
+    public byte[] PwdSalt { get; set; } = null!;
 
-    public string? FirstName { get; set; }
+    public string FirstName { get; set; } = null!;
 
-    public string? LastName { get; set; }
+    public string LastName { get; set; } = null!;
 
-    public string? PhoneNumber { get; set; }
+    public string Email { get; set; } = null!;
 
-    public string? Bio { get; set; }
+    public string? Phone { get; set; }
 
-    public int? RoleId { get; set; }
+    public int RoleId { get; set; }
 
     public virtual ICollection<AppointmentReservation> AppointmentReservations { get; set; } = new List<AppointmentReservation>();
 
@@ -35,7 +35,7 @@ public partial class User
 
     public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
 
-    public virtual Role? Role { get; set; }
+    public virtual Role Role { get; set; } = null!;
 
     public virtual ICollection<Session> Sessions { get; set; } = new List<Session>();
 
