5c6d29db89588644ce8c0d314bd4db50978be578
Tiitmouse <lkovacic380@gmail.com>
2025-01-13 19:21:55 +0100

    final for solids

---
+++ PeerTutoringNetwork/BL/Services/AppointmentService.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using BL.Models;
+using Microsoft.EntityFrameworkCore;
+
+// TODO  Interface Segregation Principle 
+public interface IAppointmentService
+{
+    Task<List<Appointment>> GetAppointments();
+    Task<bool> DeleteAppointment(int id);
+}
+public class AppointmentService : IAppointmentService
+{
+    private readonly PeerTutoringNetworkContext _context;
+
+    public AppointmentService(PeerTutoringNetworkContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Appointment>> GetAppointments()
+    {
+        var appointments = await _context.Appointments
+             .Include(a => a.AppointmentReservations)
+             .Include(a => a.Mentor)
+             .Include(a => a.Subject)
+             .ToListAsync();
+        return appointments;
+    }
+
+    // TODO  Single Responsibility Principle
+    public async Task<bool> DeleteAppointment(int id)
+    {
+        var appointment = await _context.Appointments.FindAsync(id);
+        if (appointment == null)
+        {
+            return false;
+        }
+
+        _context.Appointments.Remove(appointment);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+}
